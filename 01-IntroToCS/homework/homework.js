95ec970da5fd6ce2767df50d769e70759fae2867
juliocbarreiro <juliocbarreiro@gmail.com>
2022-08-03 09:29:48 -0300

    Homework ok

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  
+  let numDecimal = 0;
+  for(let i = num.length - 1; i >= 0 ; i--){
+    numDecimal += (num[i] * 2 ** (num.length -1 -i));
+    
+  }   
+  
+ return numDecimal;
 
 }
 
+
 function DecimalABinario(num) {
   // tu codigo aca
-
+let binario = "";
+let result = 0;
+for(let i = num; i >=0; i--){
+  if(num != 0){
+    binario = (num % 2) + binario;
+    result = Math.floor (num / 2);
+    num = result;
+    i = num;
+  }
+}
+return binario;
 }
 
 
